a9ba9faf2113fd40413790bd346d8d2a78481be5
Pulkit jindal <58661723+Pul-kt@users.noreply.github.com>
2025-06-05 00:20:50 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (79.65%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
+//Approach 2
     int trap(vector<int>& height) {
-        int n = height.size();
-        if (n == 0) return 0;
+    int left = 0, right = height.size() - 1;
+    int leftMax = 0, rightMax = 0;
+    int water = 0;
 
-        vector<int> leftMax(n), rightMax(n);
-        int water = 0;
-
-        // Fill leftMax
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; ++i)
-            leftMax[i] = max(leftMax[i - 1], height[i]);
-
-        // Fill rightMax
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; --i)
-            rightMax[i] = max(rightMax[i + 1], height[i]);
-
-        // Calculate trapped water
-        for (int i = 0; i < n; ++i)
-            water += min(leftMax[i], rightMax[i]) - height[i];
-
-        return water;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) leftMax = height[left];
+            else water += leftMax - height[left];
+            left++;
+        } else {
+            if (height[right] >= rightMax) rightMax = height[right];
+            else water += rightMax - height[right];
+            right--;
+        }
+    }
+    return water;
     }
 };
